30ac9eb380bc31abdcd88e0286fc6c66016aced7
RAAJ <rajaratnam.katta@gmail.com>
2023-08-29 21:15:44 +0530

    Delete java/com/userreg/UserRegistration directory

--- java/com/userreg/UserRegistration/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.userreg.UserRegistration.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.userreg.UserRegistration.entity.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-	 Role findByName(String name);
-}
